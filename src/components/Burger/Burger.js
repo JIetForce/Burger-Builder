5d52c8548476b3b1dc8a2d0990214fcfa08f0bd2
JIetForce <45129533+JIetForce@users.noreply.github.com>
2020-01-03 22:56:40 +0200

    Added new features

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -2,12 +2,25 @@ import React from 'react';
 
 import classes from './Burger.css';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
+
 const burger = ( props ) => {
+  let transformerIngredients = Object.keys(props.ingredients)
+  .map(igKey => {
+    return [...Array(props.ingredients[igKey])].map((_, i) => {
+      return <BurgerIngredient key={igKey + i} type={igKey} />
+    })
+  }).reduce((arr, el) => {
+    return arr.concat(el);
+  }, []);
+
+  if(transformerIngredients.length === 0) {
+    transformerIngredients = <p>Please start adding ingredients</p>
+  }
+  
   return(
     <div className={classes.Burger}>
       <BurgerIngredient type='bread-top' />
-      <BurgerIngredient type='cheese' />
-      <BurgerIngredient type='meat' />
+      {transformerIngredients}
       <BurgerIngredient type='bread-bottom' />
     </div>
   );
